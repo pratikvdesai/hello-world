857571f371d3e6f99b9ebec9f1d484c14780002c
Pratik Desai <Pratik.Desai1@xpo.com>
2019-02-05 13:00:53 -0500

    addingchanges

---
+++ LearnGit/TestGitIgnore/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestGitIgnore.Startup))]
+namespace TestGitIgnore
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
